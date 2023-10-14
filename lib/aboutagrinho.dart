1986efe03d663aa6cd5fdb9868ef7c9a01008ef3
leonardo <lcostaaraujo9@gmail.com>
2023-10-14 12:01:24 -0300

    Erro de privacy

--- lib/aboutagrinho.dart
+++ lib/aboutagrinho.dart
@@ -13,19 +13,19 @@ class InfoAgrinho extends StatelessWidget {
             children: [
               Text("Programa Agrinho",
               textAlign: TextAlign.justify,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
               
               Text("O Agrinho é um programa de responsabilidade social do Serviço de Aprendizagem Rural SENAR/CE",
               textAlign: TextAlign.justify,
-              style: TextStyle(fontSize: 16),),
+              style: TextStyle(fontSize: 20),),
               
               Text("Objetivo",
               textAlign: TextAlign.justify,
-              style: TextStyle(fontWeight: FontWeight.bold,  fontSize: 16),),
+              style: TextStyle(fontWeight: FontWeight.bold,  fontSize: 20),),
               
               Text("Despertar a conciência de cidadania nas crianças e jovens em fase escolar, tedno a Ética e a Sustentabilidade como linhas condutoras de abordagem",
               textAlign: TextAlign.justify,
-              style: TextStyle(fontSize: 16)),
+              style: TextStyle(fontSize: 20)),
 
             ],
           ),
